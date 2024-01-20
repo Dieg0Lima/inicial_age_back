d78e6c032e7906de11450691d8f031bb40e7ba53
Diego Costa LIma <Dieg0LIma>
2024-01-20 19:12:46 -0300

    teste

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost', '127.0.0.1', '192.168.1.0'
-    resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+    origins 'localhost:8080', '127.0.0.1:8080'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
