a278644b8262dc41cb2e45923e32eedc5094601e
Diego Costa LIma <Dieg0LIma>
2024-01-20 21:01:39 -0300

    teste1

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,13 +1,11 @@
 class Contract < ApplicationRecord
-  # Associações (ajuste-as conforme seu modelo)
   belongs_to :client, class_name: 'Person', foreign_key: 'client_id'
   belongs_to :people_address
   has_many :contract_items
   has_many :service_products, through: :contract_items
 
-  # Supondo que estas associações estejam definidas em seus respectivos modelos
 
-  def self.custom_query(client_name = nil, page: 1, per_page: 10)
+  def self.custom_query(client_name = nil)
     contracts = Contract.joins(:client, :people_address, :service_products)
                         .select(
                           'contracts.contract_number AS contrato',
@@ -25,6 +23,6 @@ class Contract < ApplicationRecord
     contracts = contracts.where('clients.name ILIKE ?', "%#{client_name}%") if client_name.present?
     contracts = contracts.where('service_products.title ILIKE ?', '%plano%')
 
-    Kaminari.paginate_array(contracts).page(page).per(per_page)
+    contracts
   end
 end
