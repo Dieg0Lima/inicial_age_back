4075d8d8b4d031662406767ab69820f6ae37d1f9
Dieg0CLima <diego.lima@agetelecom.com.br>
2024-02-29 17:23:26 -0300

    produção cb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
-config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:8080', '127.0.0.1:8080'
-    resource '*', headers: :any, methods: [:get, :post, :options]
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
