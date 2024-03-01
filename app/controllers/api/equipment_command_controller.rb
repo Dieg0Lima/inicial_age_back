bfbb1d40ce470b5a4339f48b4670f831d91b7882
Dieg0CLima <diego.lima@agetelecom.com.br>
2024-03-01 10:31:00 -0300

    produção

--- app/controllers/api/equipment_command_controller.rb
+++ app/controllers/api/equipment_command_controller.rb
@@ -1,7 +1,7 @@
 module Api
 class EquipmentCommandController < ApplicationController
   include HTTParty
-  base_uri 'http://192.168.69.80:3000'
+  base_uri 'https://ageatende'
 
   def execute_command
     command = params[:command]
