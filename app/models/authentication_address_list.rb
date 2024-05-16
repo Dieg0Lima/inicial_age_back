57ee4c38857012f0776f71b5eceb7fa3dfe8d8bd
Diego Costa Lima <diego.lima@agetelecom.com.br>
2024-05-16 08:20:54 -0300

    att

--- app/models/authentication_address_list.rb
+++ app/models/authentication_address_list.rb
@@ -1,4 +1,7 @@
 class AuthenticationAddressList < VoalleDataBase
   self.inheritance_column = :_type_disabled
+  self.ignored_columns = ["hash"]
+  alias_method :hash_key, :hash
+
   has_many :authentication_contract
 end
